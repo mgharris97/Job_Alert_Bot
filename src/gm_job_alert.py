45a944e754ed1309eda56cd151b158dd610ec5e1
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-11-09 13:57:19 +0200

    Test sns

--- src/gm_job_alert.py
+++ src/gm_job_alert.py
@@ -1,22 +1,28 @@
-from dotenv import load_dotenv
+#import load_dotenv function from python-dotenv library
+from dotenv import load_dotenv 
 import os
-from twilio.rest import Client   # ← this import is crucial!
-
-# Load .env variables
+## Import CLient class from Twilio's REST API 
+from twilio.rest import Client
+import requests
+from bs4 import BeautifulSoup
 load_dotenv()
 
-account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+account_sid = os.getenv("TWILIO_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 twilio_phone = os.getenv("TWILIO_PHONE")
 my_phone = os.getenv("MY_PHONE")
 
+#Twilio client initiate
 client = Client(account_sid, auth_token)
 
-# test sms
+#Test SMS
 message = client.messages.create(
-    body="Test message from Job Alert Bot!",
-    from_=twilio_phone,
-    to=my_phone
-)
+    body = "This is a test message",
+    from_=twilio_phone, to=my_phone)
 
-print("✅ Message sent! SID:", message.sid)
+#print ("Message was sent using Twilio number")
+
+#def fetch_jobs():
+#   url = os.getenv("URL")
+
+
